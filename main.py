75554d41ab439524bbd37ae0664f725938cf47c7
Lazer kat <165744889+lazerkatsweirdstuff@users.noreply.github.com>
2026-06-11 17:42:49 +1200

    hi

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "hi"
+    return render_template('index.html')
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=1234, debug=True)
